83c773166276e472422bb85e28958bcbb83a0b0a
Andrew Garrett <athom@MacBook-inator.local>
2018-12-28 22:49:42 -0600

    Moved home styles from stylesheets folder to home folder. Took import out of index.html. Changed a couple of classNames to be more specific.

--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ButtonToolbar, Button } from 'react-bootstrap';
+import './styles.css'
 
 class Home extends Component {
   render() {
@@ -15,16 +16,16 @@ class Home extends Component {
         </div>
         <hr/>
         <div className="aboutContainer">
-          <h3 className="aboutUs"> ABOUT US </h3>
-          <p className="aboutMe">
+          <h3 className="aboutTitle"> ABOUT US </h3>
+          <p className="aboutContent">
             Own Up Grown Up is a scheduling and reminder application that bridges the gap between being an adult and actually remembering what adults have to do. Own Up Grown Up's platform makes scheduling all your "adulting" needs fast and reliable.  From car maintenance to home maintenance, Own Up Grown Up has all your scheduling and reminder needs.
           </p>
           <br/>
-          <p className="aboutMe">
+          <p className="aboutContent">
             Own Up Grown Up was created by TEAM JACK (Julianne Peters, Andrew Garrett, Chris Stubbs, and Kristen McCloud) and includes professional design and security.  Using our application and the Twilio API, you will always get your reminder and always be able to change your personal preferences.
             </p>
             <br/>
-            <p className="aboutMe">
+            <p className="aboutContent">
             Don't forget to remember, or just use Own Up Grown Up.
           </p>
         </div>
